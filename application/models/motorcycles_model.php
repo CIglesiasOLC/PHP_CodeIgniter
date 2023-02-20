f97a01552ae12c22f5c46b6eea9b93afe68bf2de
CIglesiasOLC <christian.iglesias@ama.edu.ph>
2023-02-20 23:16:12 +0800

    Implementing MVC

--- application/models/motorcycles_model.php
+++ application/models/motorcycles_model.php
@@ -1,5 +1,5 @@
 <?php
-class Motorcycles_moded extends CI_Model{
+class Motorcycles_model extends CI_Model{
     public function __construct(){
         parent::__construct();
         $this->load->database();
