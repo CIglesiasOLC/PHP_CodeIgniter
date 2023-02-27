c97a527de88c9f439166713aa78573964bb7db4f
CIglesiasOLC <christian.iglesias@ama.edu.ph>
2023-02-27 23:44:17 +0800

    Custom Validation and Callbacks

--- application/controllers/Motorcycles.php
+++ application/controllers/Motorcycles.php
@@ -16,7 +16,7 @@ class Motorcycles extends CI_Controller{
     public function add(){
         $data["title"] = "Add Motorcycle Record";
 
-        $this->form_validation->set_rules("brand", "Brand", "required");
+        $this->form_validation->set_rules("brand", "Brand", "required|callback_brand_check");
         $this->form_validation->set_rules("model", "Model", "required");
         $this->form_validation->set_rules("displacement", "Displacement", "required");
 
@@ -25,6 +25,15 @@ class Motorcycles extends CI_Controller{
         }
         else {
             $this->load->view("pages/motorcycle_add", $data);
+        }
+    }
+
+    public function brand_check($brand){
+        if($brand == "xyz"){
+            $this->form_validation->set_message("brand_check", "This %s is banned and not honored.");
+            return false;
+        }else {
+            return true;
         }
     }
 }
