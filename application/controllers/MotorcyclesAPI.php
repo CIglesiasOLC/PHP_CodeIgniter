3f6a76640ea4b368a9f9038cf05056846a103069
CIglesiasOLC <christian.iglesias@ama.edu.ph>
2023-03-29 23:38:48 +0800

    Create a Motorcycle API

---
+++ application/controllers/MotorcyclesAPI.php
@@ -0,0 +1,13 @@
+<?php
+class MotorcyclesAPI extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function index(){
+        $this->load->model("Motorcycles_model");
+        $motorcycles = $this->Motorcycles_model->get_motorcycles();
+
+        echo json_encode($motorcycles);
+    }
+}
