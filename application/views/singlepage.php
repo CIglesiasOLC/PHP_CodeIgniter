7d0d2f8f3f03aabf84443a6eb0c3e0f43b33af6a
CIglesiasOLC <christian.iglesias@ama.edu.ph>
2023-03-29 23:34:50 +0800

    Setting up jQuery

--- application/views/singlepage.php
+++ application/views/singlepage.php
@@ -2,7 +2,13 @@
 <html>
 <head>
     <title>Single Page App</title>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-3.6.4.min.js'); ?>"></script>
 </head>
 <body>
     <h1>Single Page Motorcycle Listing</h1>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            alert("jQuery is ready!");
+        });
+    </script>
 </body>
