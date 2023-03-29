0678cb70db4d23286064e10b3e82149814ca6eb6
CIglesiasOLC <christian.iglesias@ama.edu.ph>
2023-03-29 23:47:08 +0800

    Implementing Datatable

--- application/views/singlepage.php
+++ application/views/singlepage.php
@@ -2,13 +2,29 @@
 <html>
 <head>
     <title>Single Page App</title>
+    <link href="<?php echo base_url('assets/dataTables/datatables.min.css'); ?>" rel="stylesheet"/>
+
     <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-3.6.4.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/dataTables/datatables.min.js'); ?>"></script>
 </head>
 <body>
     <h1>Single Page Motorcycle Listing</h1>
+    <table id="motorcycle-table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Brand</th>
+                <th>Model</th>
+                <th>Displacement</th>
+                <th>Action</th>
+            </tr>   
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
     <script type="text/javascript">
         $(document).ready(function(){
-            alert("jQuery is ready!");
+            $("#motorcycle-table").DataTable();
         });
     </script>
 </body>
